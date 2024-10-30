67a52ce7d1c4eaa9601761ebb0f6ce93e829e59c
Rupak182 <rupakgoura18@gmail.com>
2024-10-30 11:11:13 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Impact from "./components/Impact"
-import Inpact from './components/Impact'
 export default function App() {
   return (
     <div className="flex flex-col gap-3 bg-[#f4f6f9] !scroll-smooth ">
