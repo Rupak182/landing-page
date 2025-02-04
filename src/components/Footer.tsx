000961cfe798a8bb01c8db9e1edec17f7af10702
Rupak182 <rupakgoura18@gmail.com>
2025-02-04 20:38:32 +0530

    changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <div className='flex flex-col p-14 py-0 '>
+    <div className='flex flex-col p-14 py-0  '>
         <div className="p1 flex justify-around pb-4 border-b-2 border-gray-200">
             <div className='flex flex-col gap-3'>
                 <span className='font-semibold mb-1'>COMPANY</span>
@@ -27,8 +27,8 @@ export default function Footer() {
             </div>
         </div>
 
-        <div className=' flex md:gap-10 gap-5 justify-center w-full mt-5 '>
-            <img className='self-start' src='logo.svg'></img><span className='flex items-center justify-center'>Copyright &copy; 2024 Metreecs</span>
+        <div className=' flex md:gap-10 gap-5 justify-center w-full m-5 py-5 '>
+            <img className='self-start' src='logo.svg'></img><span className='flex items-center justify-center'>Copyright &copy; 2024 Test</span>
         </div>
     </div>
   )
